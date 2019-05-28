c5feb3c95a7df0f49e0cc18d2962564e1449999e
isaac1104 <isaac-kwon@hotmail.com>
2019-05-28 15:26:12 +0900

    fetch data on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchIssData } from './actions';
 
-const App = () => {
-  return (
-    <h1>App</h1>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchIssData();
+  }
+
+  render() {
+    console.log(this.props.iss_data);
+    return (
+      <h1>App</h1>
+    );
+  }
+}
+
+const mapStateToProps = ({ iss_data }) => {
+  return {
+    iss_data
+  };
 };
 
-export default App;
+export default connect(mapStateToProps, { fetchIssData })(App);
