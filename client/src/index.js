339d16bea24f9fd4f53e5d19254d22234322f54d
isaac1104 <isaac-kwon@hotmail.com>
2019-05-28 16:58:33 +0900

    add index.css for body style

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
