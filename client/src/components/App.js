9961dd5344e05fdde9a9211267fb34bdd78b49fa
isaac1104 <isaac-kwon@hotmail.com>
2019-05-28 18:07:13 +0900

    lazyload pigeonmap

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import PigeonMap from './PigeonMap/PigeonMap';
+import React, { Component, Suspense, lazy } from 'react';
+import Loader from './Loader/Loader';
+
+const PigeonMap = lazy(() => import('./PigeonMap/PigeonMap'));
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Suspense fallback={<Loader />}>
         <PigeonMap />
-      </>
+      </Suspense>
     );
   }
 }
